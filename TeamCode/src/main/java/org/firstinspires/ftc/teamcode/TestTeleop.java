b5cfce871d4b8e7ae79bc09358d60f8cc0a825d0
Tainin <jakeeli52@gmail.com>
2017-09-20 15:37:01 -0400

    Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "RunTest")
+public class TestTeleop extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        waitForStart();
+
+        while (opModeIsActive()){
+
+
+            idle();
+        }
+
+
+    }
+}
