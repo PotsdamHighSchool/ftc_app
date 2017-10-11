9e894ac21e8538a20d5fdb90d0dec78eef0b58ff
bpatterson <jheuser742@potsdam.k12.ny.us>
2017-10-11 15:22:05 -0400

    f the phone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -1,22 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 @TeleOp(name = "RunTest")
-public class TestTeleop extends LinearOpMode {
+public class TestTeleop extends OpMode {
+
+    DcMotor frontLeft, frontRight, backLeft, backRight;
+
+
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void init() {
+
+        frontLeft = (DcMotor) hardwareMap.get("mapFrontLeft");
+        frontRight = (DcMotor) hardwareMap.get("mapFrontRight");
+        backLeft = (DcMotor) hardwareMap.get("mapBackLeft");
+        backRight = (DcMotor) hardwareMap.get("mapBackRight");
 
 
-        waitForStart();
+        telemetry.addData("Status", "Initialized");
 
-        while (opModeIsActive()){
+    }
 
-            //Test2
-            idle();
-        }
+    @Override
+    public void loop() {
+
+        if (gamepad1.a){
+            backLeft.setPower(0.5);
+        } else backLeft.setPower(0);
+
+        if (gamepad1.b){
+            backRight.setPower(0.5);
+        } else backRight.setPower(0);
+
+        if (gamepad1.x){
+            frontLeft.setPower(0.5);
+        } else frontLeft.setPower(0);
+
+        if (gamepad1.y){
+            frontRight.setPower(0.5);
+        } else frontRight.setPower(0);
 
 
     }
