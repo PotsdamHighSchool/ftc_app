6c40c24fc0008fb777a25f7a8ce7373a640e1952
bpatterson <jheuser742@potsdam.k12.ny.us>
2017-10-18 16:14:12 -0400

    IT WORKS!!! (kind of...)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 
 @TeleOp(name = "RunTest")
@@ -18,31 +19,26 @@ public class TestTeleop extends OpMode {
         frontRight = (DcMotor) hardwareMap.get("mapFrontRight");
         backLeft = (DcMotor) hardwareMap.get("mapBackLeft");
         backRight = (DcMotor) hardwareMap.get("mapBackRight");
-
-
-        telemetry.addData("Status", "Initialized");
-
     }
 
     @Override
     public void loop() {
+        doDrive();
+    }
 
-        if (gamepad1.a){
-            backLeft.setPower(0.5);
-        } else backLeft.setPower(0);
+    private void doDrive() {
+        double drive = gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double rotate = gamepad1.right_stick_x;
 
-        if (gamepad1.b){
-            backRight.setPower(0.5);
-        } else backRight.setPower(0);
+        double FL = drive + strafe + rotate;
+        double FR = drive - strafe - rotate;
+        double BL = drive - strafe  + rotate;
+        double BR = drive + strafe - rotate;
 
-        if (gamepad1.x){
-            frontLeft.setPower(0.5);
-        } else frontLeft.setPower(0);
-
-        if (gamepad1.y){
-            frontRight.setPower(0.5);
-        } else frontRight.setPower(0);
-
-
+        frontLeft.setPower(Range.clip(FL, -1, 1));
+        frontRight.setPower(Range.clip(FR, -1, 1));
+        backLeft.setPower(Range.clip(BL, -1, 1));
+        backRight.setPower(Range.clip(BR, -1, 1));
     }
 }
